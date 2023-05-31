aba3d203b6c5059c74d700531a523a46e518c4fb
ALukyanov <lukyanov@v-mix.ru>
2023-05-31 12:09:47 +0300

    model updated

--- Models/WarehouseToSellerModel.py
+++ Models/WarehouseToSellerModel.py
@@ -3,7 +3,8 @@ from BaseModel import *
 
 class WarehouseToSeller(Base):
     __tablename__ = 'WarehouseToSeller'
-    WarehousePartID = Column(Integer, ForeignKey('Warehouse.PartID'), primary_key=True)
-    SellerID = Column(Integer, ForeignKey('Sellers.SellerID'), primary_key=True)
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    WarehousePartID = Column(Integer, ForeignKey('Warehouse.PartID'), nullable=False)
+    SellerID = Column(Integer, ForeignKey('Sellers.SellerID'), nullable=False)
     seller = relationship('Sellers', backref='warehouse_to_seller')
     warehouse = relationship('Warehouse', backref='warehouse_to_seller')
