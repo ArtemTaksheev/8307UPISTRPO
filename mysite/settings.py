db50317e7c1ca65d2b16eca477588aa19b4b86b3
Vladis759 <45987960+Vladis759@users.noreply.github.com>
2023-06-02 00:55:48 +0300

    simple sql test + req for venv + moved directories back cause im stupid + ratio + your localhost + no csss

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,9 +138,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join('static'),
-)
+
+STATICFILES_DIRS = []
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
