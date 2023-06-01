4e8726aee584aedc10be6afe806734c44527a988
Vladis759 <45987960+Vladis759@users.noreply.github.com>
2023-06-01 20:11:41 +0300

    simple sql test + req for venv + moved directories

--- Models/PriceListModel.py
+++
@@ -1,8 +0,0 @@
-from BaseModel import *
-
-
-class PriceList(Base):
-    __tableName__ = 'PriceList'
-    PartServiceID = Column(Integer, primary_key=True, autoincrement=True)
-    PartServiceName = Column(String(100), nullable=False)
-    Price = Column(Integer, nullable=False)
